8edbfaf6faf37a51cb9fea0108ec8286fb7a8466
Felandric <abuguchie@yahoo.com>
2018-03-25 00:15:49 -0400

    added cookies module

--- TechChefs/orderQ/src/app/cust-components/entrees/entrees.component.spec.ts
+++ TechChefs/orderQ/src/app/cust-components/entrees/entrees.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ngCookies } from angular-cookies 
 import { EntreesComponent } from './entrees.component';
 
 describe('EntreesComponent', () => {
