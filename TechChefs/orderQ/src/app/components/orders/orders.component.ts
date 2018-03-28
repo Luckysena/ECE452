eddb733fdb9066ff9e3df08437c77819071e3698
newtonianguy <newtonianguy123@yahoo.com>
2018-03-28 14:57:45 -0400

    Updated Order Queue Timer

--- TechChefs/orderQ/src/app/components/orders/orders.component.ts
+++ TechChefs/orderQ/src/app/components/orders/orders.component.ts
@@ -12,16 +12,9 @@ import {DataService} from '../../services/data.service';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
-  orders: AngularFireList<any[]>;
-  index: number = 0;
-	seats: number[];
-	entrees: string[];
-	sides: string[];
+	orders: AngularFireList<any[]>;
 	isSearch: boolean = false;
 	ordObservable: Observable<any[]>;
-	ordObservable1: Observable<any[]>;
-	ordObservable2: Observable<any[]>;
-	ordObservable3: Observable<any[]>;
   constructor(private db: AngularFireDatabase, private router: Router, private dServe: DataService) {
 
 	}
@@ -57,49 +50,64 @@ export class OrdersComponent implements OnInit {
   }
 }
 //JS Timer for the tickets
-function initTime(){
-  var iTime = new Date();
-  var iM = iTime.getTime();
-  return iM;
-}
-var iMill = initTime();
-var c = document.getElementsByClassName("clock");
-var j = 0;
-//dataRet();
-setInterval(function(){myTime(j)}, 1000);
-//var iMill = [];
-while(j<c.length)
-{
-  //iMill[j] = initTime();
-  myTime(j);
-  j++;
-}
+var iMill = document.getElementsByClassName("clock");//gets initial timestamp of order arrival
+var initialize = true;//helps initialize time counter
 
-function myTime(j) {
-  var cTime = new Date();
-  var cMill = cTime.getTime();
-  var tMill = cMill - iMill;
-  var tSec = Math.round(tMill/1000);
-  var t2Sec = ++tSec%60;
-  var tMin = Math.floor(tSec/60);
-  var sec = t2Sec.toString();
-  var min = tMin.toString();
-  min = checkTime(min);
-  sec = checkTime(sec);
-  for(var i =0; i < c.length; i++){
-    document.getElementsByClassName("clock")[i].innerHTML = min + ":" + sec;
-    let  colElements = document.getElementsByClassName("column") as HTMLCollectionOf<HTMLElement>;
-    if(tMin<7){
-      colElements[i].style.backgroundColor = "white";
-    }
-    else if(tMin>=7&&tMin<10){
-      colElements[i].style.backgroundColor = "yellow";
-    }
-    else if(tMin>=10){
-      colElements[i].style.backgroundColor = "red";
-    }
-  }
+function myTime() {
+	//gets current time
+	var cTime = new Date();
+	var cMill = cTime.getTime();
+	//takes initial time stamp and changes it into readable data(runs once)
+	if(initialize){
+		initialize = false;
+		for(var i =0; i < iMill.length; i++){
+			var tMill = cMill - Number(iMill[i].innerHTML);
+			var tSec = Math.round(tMill/1000);
+			var t2Sec = ++tSec%60;
+			var tMin = Math.floor(tSec/60);
+			var sec1 = t2Sec.toString();
+			var min1 = tMin.toString();
+			min1 = checkTime(min1);
+			sec1 = checkTime(sec1);
+			//expresses time in mm:ss
+			document.getElementsByClassName("clock")[i].innerHTML  =  min1 + ":" + sec1;
+		}
+	}
+	//updats all orders wait time in UI
+	for(var i =0; i < iMill.length; i++){
+		//gets time from what is currently displayed
+		var pos = iMill[i].innerHTML.search(":");
+		var min = Number(iMill[i].innerHTML.slice(0,pos));
+		var sec = Number(iMill[i].innerHTML.slice(pos+1,iMill[i].innerHTML.length));
+		//increments timer
+		sec = (sec + 2);
+		if( sec >= 60 ){
+			sec = sec - 60;
+			min = min + 1;
+		}
+		var sec1 = sec.toString();
+		var min1 = min.toString();
+		min1 = checkTime(min1);
+		sec1 = checkTime(sec1);
+		//prints time
+		document.getElementsByClassName("clock")[i].innerHTML = min1 + ":" + sec1;
+		//colors orders based on how long they have been waiting
+		let  colElements = document.getElementsByClassName("column") as HTMLCollectionOf<HTMLElement>;
+		if(min<7){
+			colElements[i].style.backgroundColor = "white";
+		}
+		else if(min>=7&&min<10){
+			colElements[i].style.backgroundColor = "yellow";
+		}
+		else if(min>=10){
+			colElements[i].style.backgroundColor = "red";
+		}
+	}
 }
+//sets interval for timer 
+setInterval(function(){myTime()}, 2000);
+
+//helps format timer
 function checkTime(i) {
     if (i < 10) {
         i = "0" + i;
