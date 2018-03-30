4ab61bb4c0016b58958fc2a6f61dc82c52e897f3
umiahmed30 <umiahmed30@gmail.com>
2018-03-29 20:50:18 -0400

    new routing

--- TechChefs/orderQ/src/app/Modules/app-routing.module.ts
+++ TechChefs/orderQ/src/app/Modules/app-routing.module.ts
@@ -9,13 +9,16 @@ import { InventoryComponent } from '../components/inventory/inventory.component'
 import { ServerComponent } from '../components/server/server.component';
 import { EntreesComponent } from '../cust-components/entrees/entrees.component';
 import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 import { HomeComponent } from '../cust-components/home/home.component';
 import { PlaceOrderComponent } from '../cust-components/place-order/place-order.component';
 import { ConfirmComponent } from '../cust-components/confirm/confirm.component';
 import { CustDashboardComponent } from '../cust-components/cust-dashboard/cust-dashboard.component';
+import{EmpDashComponent} from '../components/emp-dash/emp-dash.component';
 
 const appRoutes: Routes = [
     { path: 'login', component: LoginComponent },
+        { path: 'register', component: RegisterComponent },
     {
         path: 'dashboard', component: CustDashboardComponent, children: [
             { path: 'home', component: HomeComponent },
@@ -27,6 +30,7 @@ const appRoutes: Routes = [
     { path: 'server', component: ServerComponent },
     { path: 'table/43', component: SearchorderComponent },
     { path: 'inventory', component: InventoryComponent },
+      { path: 'empDash', component: EmpDashComponent },
     { path: 'employee', component: EmployeeComponent },
     { path: 'queue', component: OrdersComponent },
     { path: 'about', component: AboutComponent },
