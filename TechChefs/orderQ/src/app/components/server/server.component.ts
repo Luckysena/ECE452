fcd810d83e51015bae0f3c3e052992df2ec94e19
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-04-29 11:42:27 -0400

    Please don't push over this again

--- TechChefs/orderQ/src/app/components/server/server.component.ts
+++ TechChefs/orderQ/src/app/components/server/server.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {DataService} from '../../services/data.service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-server',
@@ -8,15 +10,30 @@ import {DataService} from '../../services/data.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
+  servAl: AngularFireList<any[]>;
   isAlert: boolean = false;
-  alerts: Array<any>;
+  serverMess: string[] = [];
   interval: number;
+  salObservable: Observable<any[]>;
 
-  constructor(private dServe: DataService, private router: Router) {}
-
-  ngOnInit() {
+  constructor(private db: AngularFireDatabase, private dServe: DataService, private router: Router) {
+    this.servAl = db.list('/servAlert');
+    this.salObservable = this.servAl.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
   }
 
+  ngOnInit() {
+    //this.salObservable = this.getSal('/servAlert');
+  }
+  getSal(listPath): Observable<any[]> {
+		return this.db.list(listPath).valueChanges();
+  }
+  removeSal(key){
+    if(confirm("Clear this alert?")){
+      this.servAl.remove(key);
+    }
+  }
   logout(){
     this.dServe.logout();
   }
@@ -27,5 +44,3 @@ export class ServerComponent implements OnInit {
   this.isAlert=!this.isAlert;
   }
 }
-
-
