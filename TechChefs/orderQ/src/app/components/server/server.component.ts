884d911008f72dff24580e2e640bd905a2e308a8
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-04-29 20:57:13 -0400

    Update server.component.ts

--- TechChefs/orderQ/src/app/components/server/server.component.ts
+++ TechChefs/orderQ/src/app/components/server/server.component.ts
@@ -3,7 +3,7 @@ import {Router} from '@angular/router';
 import {DataService} from '../../services/data.service';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
-
+import { Table } from './table'
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
@@ -11,21 +11,29 @@ import { Observable } from 'rxjs/Observable';
 })
 export class ServerComponent implements OnInit {
   servAl: AngularFireList<any[]>;
+  tabStat: AngularFireList<any[]>;
   isAlert: boolean = false;
-  serverMess: string[] = [];
+  status: string;
   interval: number;
+  tNum: string;
   salObservable: Observable<any[]>;
+  tabObservable: Observable<any[]>;
 
   constructor(private db: AngularFireDatabase, private dServe: DataService, private router: Router) {
+    this.tabStat = db.list('/Table Status');
+    this.tabObservable = this.tabStat.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
     this.servAl = db.list('/servAlert');
     this.salObservable = this.servAl.snapshotChanges().map(changes => {
       return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
     });
   }
-
-  ngOnInit() {
-    //this.salObservable = this.getSal('/servAlert');
+  viewSched(){
+    this.router.navigate(['/entree']);
   }
+  ngOnInit() {
+    }
   getSal(listPath): Observable<any[]> {
 		return this.db.list(listPath).valueChanges();
   }
@@ -42,5 +50,35 @@ export class ServerComponent implements OnInit {
   }
   toggAlert(){
   this.isAlert=!this.isAlert;
+  }
+  tabClean(table, key, stat){
+    this.tNum = table;
+    this.status = "Table " + this.tNum + " is vacant and ready to be sat.";
+    if(this.status != stat){
+      this.tabStat.remove(key);
+      let tab = new Table(this.status, this.tNum, String(Date()));
+      this.db.list('/Table Status').push(tab);
+      location.reload();
+    }
+  }
+  tabLef(table, key, stat){
+    this.tNum = table;
+    this.status = "Table " + this.tNum + " is vacant but dirty.";
+    if(this.status != stat){
+      this.tabStat.remove(key);
+      let tab = new Table(this.status, this.tNum, String(Date()));
+      this.db.list('/Table Status').push(tab);
+      location.reload();
+    }
+  }
+  foDel(table, key, stat){
+    this.tNum = table;
+    this.status = "Table " + this.tNum + " is eating their meal.";
+    if(this.status != stat){
+      this.tabStat.remove(key);
+      let tab = new Table(this.status, this.tNum, String(Date()));
+      this.db.list('/Table Status').push(tab);
+      location.reload();
+    }
   }
 }
