acb8a68c150a355f2a7a2b5e1f4dd0c356bb2c88
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-04-29 21:06:44 -0400

    added code to update table status on placed order

---
+++ TechChefs/orderQ/src/app/cust-components/place-order/table.ts
@@ -0,0 +1,10 @@
+export class Table{
+  status: string;
+  table: string;
+  time: string;
+  constructor(status: string, table: string, time: string) {
+    this.status = status;
+    this.table = table;
+    this.time = time;
+  }
+}
