a67ccf422c688c56f88ce56d00e77331dd0fbca4
ThumTact <35500983+ThumTact@users.noreply.github.com>
2018-04-30 08:11:21 -0400

    Que?

--- TechChefs/orderQ/src/app/app.module.ts
+++ TechChefs/orderQ/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { SearchorderComponent } from './components/searchorder/searchorder.compo
 import { InventoryComponent } from './components/inventory/inventory.component';
 import { ServerComponent } from './components/server/server.component';
 import { EntreesComponent } from './cust-components/entrees/entrees.component';
-import { OrderService } from './cust-components/place-order/order.service.ts'
+import { OrderService } from './cust-components/place-order/order.service'
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
